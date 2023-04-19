4d97e11868e506fdc240c07c691f122d89a92675
Kaniskaa Behera <101510957+kaniskaab@users.noreply.github.com>
2023-04-20 00:26:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 
 import './App.css';
+import About from './components/About';
 import Contact from './components/Contact';
 import Hero from './components/Hero';
 import TechStack from './components/TechStack';
-import Timeline from './components/Timeline';
 
 function App() {
   return (
    <>
    
         <Hero/>
-        <TechStack id="elem1"/>
-        <Timeline id="elem2"/>
-        <Contact id="elem3"/>
+        <About/>
+        <TechStack/>
+        <Contact/>
+         
    </>
     
       
